52c60e5f0157cebe6b86fbf1971eec986fc91931
nghillaz <nghill2@asu.edu>
2015-04-24 12:18:14 -0700

    changed from applet to application

---
+++ desktopapplication/src/MainClass.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class MainClass {
+	public static void main(String[] args){
+		createAndShowGUI();
+	}
+	private static void createAndShowGUI(){
+	 	
+		JFrame frame = new JFrame("Doctor Portal");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Dimension initSize = new Dimension(640,480);
+		frame.setPreferredSize(initSize);
+		
+		LoginPanel loginPanel = new LoginPanel(frame.getContentPane());
+		loginPanel.setPreferredSize(initSize);
+	
+		frame.getContentPane().add(loginPanel);
+		frame.setSize(initSize);
+	    
+		//Display the window.
+        frame.pack();
+        frame.setVisible(true);
+	}
+}
