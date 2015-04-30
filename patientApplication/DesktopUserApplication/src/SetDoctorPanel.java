3633cd45f7160e8e4a008ab4339358ecaa8ff895
uhmeat <amitkulkarni43@gmail.com>
2015-04-29 20:48:41 -0700

    DoctorPanel almost done
    just needs to have a working append function

--- patientApplication/DesktopUserApplication/src/SetDoctorPanel.java
+++ patientApplication/DesktopUserApplication/src/SetDoctorPanel.java
@@ -1,9 +1,12 @@
 import java.awt.*;
+
 import javax.swing.*;
+
 import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
 
@@ -30,7 +33,7 @@ public class SetDoctorPanel extends JPanel{
 		
 		//button listeners
 		setAsDoctor.addActionListener(new SetAsDoctorListener(contentPane));
-		back.addActionListener(new BackListener(contentPane));
+		//back.addActionListener(new BackListener(contentPane));
 		
 		//setting alignment
 		setDoctor.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -125,10 +128,74 @@ public class SetDoctorPanel extends JPanel{
 			// TODO 3. Append the doctor's email address
 			// TODO 4. Upload patient.csv
 			// TODO 5. add MainMenuPanel contentPane
+			
+			PrintStream console = System.out;
+			File f1 = Database.download("doctors.csv", console);
+			File f2 = Database.download("patients.csv", console);
+			
+			String selectedDoctor = (String)selectDoctor.getSelectedItem();
+			
+			if(f1.exists() && !f1.isDirectory() && f2.exists() && !f2.isDirectory())
+			{
+				
+				BufferedReader br1;
+				BufferedReader br2;
+				FileWriter fw1;
+				FileWriter fw2;
+				
+				@SuppressWarnings("unused")
+				String head;
+				String line;
+				String patientEmail;
+				String doctorEmail = "";
+				String buffer = "";
+				
+				try {
+					   patientEmail = LoginPanel.getEmail();
+					   System.out.println(patientEmail + selectedDoctor);
+					   
+						br1 = new BufferedReader(new FileReader(f1));
+						br2 = new BufferedReader(new FileReader(f2));
+						fw1 = new FileWriter("patients.csv", true);
+						head = br1.readLine();
+						while ((line = br1.readLine()) != null ) {
+						    String[] dNames = line.split(",");
+							if(selectedDoctor.equals(dNames[1  ] + ", " + dNames[0])){
+								doctorEmail = dNames[2];
+								System.out.println(line);
+								
+								
+							}
+						}
+						while((line = br2.readLine()) != null){
+								String[] pNames = line.split(",");
+								if(pNames[2].equals(patientEmail)){
+								buffer += pNames[0]+","+pNames[1]+","+pNames[2]+","+pNames[3]+","+pNames[4]+","+doctorEmail+"\n";
+								
+								}
+								
+						}
+					br1.close();
+					System.out.println(buffer);
+					fw1.append(buffer);
+				
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			else // doctors.csv doesn't exist on the server or locally
+			{
+				JFrame frame = new JFrame();
+				JOptionPane.showMessageDialog(frame, "No doctors were found.");
+
+			}
+
+		}
 		}
 	}
 	
-	
+	/*
 	public class BackListener implements ActionListener{
 		Container contentPane;
 		public BackListener(Container contentPane){
@@ -146,3 +213,4 @@ public class SetDoctorPanel extends JPanel{
 	
 	
 }
+*/
